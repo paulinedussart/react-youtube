dcb915af499e72ddd67a6b0f04429f173825570e
Pauline Dussart <paulinedussart04@gmail.com>
2021-01-20 21:05:37 +0100

    youtube API GET work fine

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import SearchBar from './component/SearchBar';
+import youtube from './apis/youtube';
+
+const KEY = 'AIzaSyCd5gPUrQBxlFpvZ_2MLjyHw1qudLuMaGc';
+
 class App extends React.Component {
-  onSubmitForm = (e) => {
-    console.log(e);
+  onSubmitForm = (word) => {
+    console.log(word);
+    youtube.get('/search', {
+      params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        q: word,
+        key: KEY,
+      },
+    });
   };
 
   render() {
